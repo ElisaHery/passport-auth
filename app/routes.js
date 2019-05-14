0a1e1191e81c0dc65eb5552ce41b2311e43c679b
ElisaHery <elisa.hery@gmail.com>
2019-05-14 12:26:15 +0200

    initial work

---
+++ app/routes.js
@@ -0,0 +1,36 @@
+"use strict";
+// Import dependencies
+var passport = require("passport");
+var express = require("express");
+// Set up middleware
+var requireAuth = passport.authenticate("jwt", { session: false });
+
+// Export the routes for our app to use
+module.exports = function(app) {
+  // API Route Section
+
+  // Initialize passport for use
+  app.use(passport.initialize());
+
+  // Bring in defined Passport Strategy
+  require("../config/passport")(passport);
+
+  // Create API group routes
+  var apiRoutes = express.Router(); // créer un router
+  var userRoutes = require("./users/users.controller");
+
+  //Protected authenticated route with JWT
+  apiRoutes.get("/dashboard", requireAuth, function(request, response) {
+    response.send(
+      "It worked User id is: " +
+        request.user.user_id +
+        ", Email id is: " +
+        request.user.user_email +
+        "."
+    );
+  });
+
+  // Set url for API group routes
+  app.use("/api", apiRoutes); // on préfixe par /api
+  app.use("/api/users", userRoutes);
+};
